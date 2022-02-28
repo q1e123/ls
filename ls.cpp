e5bbddd7af45db12dce3d6a6ff757cc996d3e147
q1e123 <robertnicolaesolca@gmail.com>
2022-02-28 14:49:44 +0200

    added get file

--- ls.cpp
+++ ls.cpp
@@ -2,6 +2,7 @@
 #include <filesystem>
 #include <string>
 #include <vector>
+#include <iomanip>
 
 std::vector<std::string> convertToStringList(char* stringArray[], int len)
 {
@@ -14,12 +15,20 @@ std::vector<std::string> convertToStringList(char* stringArray[], int len)
 	return stringList;
 }
 
+std::string getFileName(std::filesystem::directory_entry entry)
+{
+	std::string fullPath = entry.path().string();
+	size_t startPos = fullPath.find_last_of("/\\") + 1; 
+	std::string filename = fullPath.substr(startPos);
+	return filename;
+}
+
 int main(int argc, char* argv[]) {
 	std::vector<std::string> parameterList = convertToStringList(argv, argc);
 	std::filesystem::path currentPath = std::filesystem::current_path();
 	for (const auto& entry : std::filesystem::directory_iterator(currentPath))
 	{
-		std::cout << entry.path() << std::endl;
+		std::cout << getFileName(entry) <<"\t\t" <<entry.file_size() << std::endl;
 	}
 	return 0;
 }
